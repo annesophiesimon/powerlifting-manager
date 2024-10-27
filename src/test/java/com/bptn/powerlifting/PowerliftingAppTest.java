b32b19ddb34afdfd1b0be9a665c3beebdf9b8674
Anne-Sophie SIMON <a-sophie.simon@live.fr>
2024-10-27 13:36:37 -0400

    add test

---
+++ src/test/java/com/bptn/powerlifting/PowerliftingAppTest.java
@@ -0,0 +1,64 @@
+package com.bptn.powerlifting;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import com.bptn.powerlifting.models.Barbell;
+import com.bptn.powerlifting.models.Exercice;
+import com.bptn.powerlifting.models.WorkoutLog;
+
+public class PowerliftingAppTest {
+
+	// test calculatePlateLoad class Barbell
+	// initialize object
+	private Barbell myBarbell;
+	private WorkoutLog myWorkoutLog;
+
+	@Test
+	void testCalculatePlateLoad() {
+		// create object
+		myBarbell = new Barbell(300.0);
+		myBarbell.initializePlates();
+
+		// Define the expected result map
+		Map<Double, Integer> expectedPlateLoad = new HashMap<>();
+		expectedPlateLoad.put(10.0, 1);
+		expectedPlateLoad.put(5.0, 1);
+		expectedPlateLoad.put(2.5, 1);
+		expectedPlateLoad.put(55.0, 2);
+
+		// Run the method and compare the actual result with the expected result
+		assertEquals(expectedPlateLoad, myBarbell.calculatePlateLoad(),
+				"Plate load calculation should match expected output");
+	}
+
+	@Test
+	void testGetDetailLog() {
+		// create log
+		Map<String, Exercice> log = new HashMap<>();
+		Exercice exercice1 = new Exercice(300.0, 3, LocalDate.now());
+		Exercice exercice2 = new Exercice(300.0, 3, LocalDate.now());
+
+		log.put("Squat", exercice1);
+		log.put("DeadLift", exercice2);
+
+		// create object
+		myWorkoutLog = new WorkoutLog("Squat", log);
+		// Define the expected output for getDetailsLog
+		StringBuilder expectedOutput = new StringBuilder();
+		expectedOutput.append("Workout Log:\n");
+		expectedOutput.append("Exercise: Squat\n").append(exercice1.toString()).append("\n");
+		expectedOutput.append("Exercise: DeadLift\n").append(exercice2.toString()).append("\n");
+
+		// Run the method and compare the result with the expected output
+		assertEquals(expectedOutput.toString(), myWorkoutLog.getDetailsLog(),
+				"Details log should match expected output");
+
+	}
+
+}
